5d8ddf236d351da52d87c18f7601faefafa8801a
DanilMaslenitsyn <airpronn@ya.ru>
2019-07-09 19:01:32 +0300

    1

---
+++ HomeWork2/HomeWork2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("a =");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("b =");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("c =");
+            int c = Convert.ToInt32(Console.ReadLine());
+            int min = MIN(a, b, c);
+            Console.Write(min);
+            Console.ReadLine();
+        }
+
+        private static int MIN(int a, int b, int c)
+        {
+            int min;
+            if (a < b && a < c)
+            {
+                min = a;
+            }
+            else
+            {
+                if (b < c) { min = b; }
+                else { min = c; }
+
+            }
+            return min;
+        }
+    }
+}
