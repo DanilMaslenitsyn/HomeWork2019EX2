633263c50d8f89a297e8904d3420ed99587de356
DanilMaslenitsyn <airpronn@ya.ru>
2019-07-11 22:45:22 +0300

    Update Program.cs

--- HomeWork2/HomeWork2/Program.cs
+++ HomeWork2/HomeWork2/Program.cs
@@ -12,33 +12,76 @@ namespace HomeWork2
         {
             //Задание 1
 
-        //    Console.Write("a =");
-        //    int a = Convert.ToInt32(Console.ReadLine());
-        //    Console.Write("b =");
-        //    int b = Convert.ToInt32(Console.ReadLine());
-        //    Console.Write("c =");
-        //    int c = Convert.ToInt32(Console.ReadLine());
-        //    int min = MIN(a, b, c);
-        //    Console.Write(min);
-        //    Console.ReadLine();
-        //}
+            //    Console.Write("a =");
+            //    int a = Convert.ToInt32(Console.ReadLine());
+            //    Console.Write("b =");
+            //    int b = Convert.ToInt32(Console.ReadLine());
+            //    Console.Write("c =");
+            //    int c = Convert.ToInt32(Console.ReadLine());
+            //    int min = MIN(a, b, c);
+            //    Console.Write(min);
+            //    Console.ReadLine();
+            //}
 
-        //private static int MIN(int a, int b, int c)
-        //{
-        //    int min;
-        //    if (a < b && a < c)
-        //    {
-        //        min = a;
-        //    }
-        //    else
-        //    {
-        //        if (b < c) { min = b; }
-        //        else { min = c; }
+            //private static int MIN(int a, int b, int c)
+            //{
+            //    int min;
+            //    if (a < b && a < c)
+            //    {
+            //        min = a;
+            //    }
+            //    else
+            //    {
+            //        if (b < c) { min = b; }
+            //        else { min = c; }
 
-        //    }
-        //    return min;
+            //    }
+            //    return min;
 
             //Задание 2
+
+            //    Console.Write("a =");
+            //    int a = Convert.ToInt32(Console.ReadLine());
+
+            //    int count = Counter( a);
+            //    Console.WriteLine(count);
+            //    Console.ReadLine();
+
+            //}
+
+            //private static int Counter(int a)
+            //{
+            //    int count = 0;
+            //    if (a == 0) count = 1;
+            //    while (a != 0)
+            //    {
+            //        a = a / 10;
+            //        count++;
+
+            //    }
+
+            //    return count;
+
+            // Задание 3
+
+
+            
+        // Sun1();
+           
+            
+        //}
+
+        //private static void Sun1()
+        //{
+        //    int sun = 0;
+        //    while (true)
+        //    {
+        //        Console.Write("a =");
+        //        int a = Convert.ToInt32(Console.ReadLine());
+
+        //        if (a == 0) break;
+        //        if (a > 0 && a % 2 == 1) ; sun += a;
+        //    }
         }
     }
 }
